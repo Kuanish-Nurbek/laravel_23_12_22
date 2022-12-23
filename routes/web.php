7495acd4493b4a76176fb1db31b17a46b3e6ab6d
nurbek <nurbekkuanish031295@gmail.com>
2022-12-23 14:41:54 +0600

    <5commit>

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/abc', function () {
+    return view('home');
+});
+
 // Route::get('/', function () {
 //     return view('home');
 // });
